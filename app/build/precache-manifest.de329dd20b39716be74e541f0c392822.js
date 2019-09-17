ac655ae5b6ef1bc02ce24aa9144758268b979c6c
Skhumbuzo <skhumbuzo.mkhungo@allangray.co.za>
2019-09-17 08:31:16 +0200

    Added build files.

---
+++ app/build/precache-manifest.de329dd20b39716be74e541f0c392822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27162cb641852309b8f513ea47a3d434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3a226bcefdd2f72ed9",
+    "url": "/static/css/main.e400ee56.chunk.css"
+  },
+  {
+    "revision": "50092569e8b63addb95c",
+    "url": "/static/js/2.7c314d9f.chunk.js"
+  },
+  {
+    "revision": "7e3a226bcefdd2f72ed9",
+    "url": "/static/js/main.72e2b563.chunk.js"
+  },
+  {
+    "revision": "f4470a1d358b84166157",
+    "url": "/static/js/runtime~main.edc4bf8c.js"
+  },
+  {
+    "revision": "fedc2c7f11fe7c5601132c70e8b588d8",
+    "url": "/static/media/FFSALOGOS-01.fedc2c7f.png"
+  }
+]);
